e699c99ba4d40db3492fe1a783540e4ab86b8882
lxxxxxxx7 <2630484635@qq.com>
2024-10-15 22:22:56 +0800

    init: 初始化项目结构

---
+++ Sevice/src/main/java/com/lx/songjoyhub/joyservice/JoyServiceApplication.java
@@ -0,0 +1,11 @@
+package com.lx.songjoyhub.joyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JoyServiceApplication.class, args);
+    }
+}
