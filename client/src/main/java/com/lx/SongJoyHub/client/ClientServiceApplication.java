2a9f918681de2d96b3aa577407b470ea3d126ae2
lxxxxxxx7 <2630484635@qq.com>
2024-10-22 10:44:00 +0800

    optimize: SpringBootApplication添加包扫描路径 避免引用framework中的Bean生效

--- client/src/main/java/com/lx/SongJoyHub/client/ClientServiceApplication.java
+++ client/src/main/java/com/lx/SongJoyHub/client/ClientServiceApplication.java
@@ -3,7 +3,7 @@ package com.lx.SongJoyHub.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.lx.SongJoyHub"})
 public class ClientServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClientServiceApplication.class, args);
