29920218bf2b468214538376c4bb8f1d830cd112
lxxxxxxx7 <2630484635@qq.com>
2024-10-24 18:15:29 +0800

    feature: 开发审批歌曲功能

--- music/src/main/java/com/lx/SongJoyHub/music/controller/SongReviewController.java
+++ music/src/main/java/com/lx/SongJoyHub/music/controller/SongReviewController.java
@@ -1,6 +1,12 @@
 package com.lx.SongJoyHub.music.controller;
 
+import com.lx.SongJoyHub.framework.result.Result;
+import com.lx.SongJoyHub.framework.web.Results;
+import com.lx.SongJoyHub.music.dto.req.SongReviewReqDTO;
+import com.lx.SongJoyHub.music.serivce.SongReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/song-review")
 @RequiredArgsConstructor
 public class SongReviewController {
+
+    private final SongReviewService songReviewService;
+    /**
+     * 歌曲审批
+     */
+    @PostMapping("/examine")
+    public Result<Void> songExamine(@RequestBody SongReviewReqDTO requestParam) {
+        songReviewService.songExamine(requestParam);
+        return Results.success();
+    }
 }
